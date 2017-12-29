ed3bc5166b8047ee3498b3fc5de1784d076c2e91
Marc Gravell <marc.gravell@gmail.com>
2017-12-29 12:07:58 +0000

    proper RESP parser

--- RedisPing/RedisPing.cs
+++ RedisPing/RedisPing.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets;
 using Newtonsoft.Json;
+using RedisPing;
 using System;
 using System.Buffers;
 using System.Buffers.Text;
@@ -13,7 +14,7 @@ using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
-static class RedisPing
+namespace RedisPing
 {
     class TestCase
     {
@@ -24,232 +25,230 @@ static class RedisPing
         public bool UseTls { get; set; }
         public string CertificatePath { get; set; }
     }
-    private static bool ShowDetails
-    {
-        get
+    static class Program {
+        private static bool ShowDetails
         {
+            get
+            {
 #if DEBUG
-            return true;
+                return true;
 #else
             return false;
 #endif
+            }
         }
-    }
-    static async Task Main()
-    {
-        var testRoot = "Tests";
-        using (var pool = new MemoryPool())
+        static async Task Main()
         {
-            foreach (var path in Directory.EnumerateFiles(testRoot, "*.json"))
+            var testRoot = "Tests";
+            using (var pool = new MemoryPool())
             {
-                try
+                foreach (var path in Directory.EnumerateFiles(testRoot, "*.json"))
                 {
-                    var json = File.ReadAllText(path);
-                    var test = JsonConvert.DeserializeObject<TestCase>(json);
-                    X509Certificate cert = null;
-                    if (test.CertificatePath != null)
+                    try
                     {
-                        cert = new X509Certificate2(Path.Combine(testRoot, test.CertificatePath));
+                        var json = File.ReadAllText(path);
+                        var test = JsonConvert.DeserializeObject<TestCase>(json);
+                        X509Certificate cert = null;
+                        if (test.CertificatePath != null)
+                        {
+                            cert = new X509Certificate2(Path.Combine(testRoot, test.CertificatePath));
 
+                        }
+
+                        await Console.Out.WriteLineAsync($"Test: {test.Name ?? test.Host}");
+                        if (cert != null)
+                        {
+                            await Console.Out.WriteLineAsync($"Client certificate: {cert.Subject}");
+                        }
+
+                        await Console.Error.WriteLineAsync("via TcpClient...");
+                        await DoTheThingViaTcpClient(pool, test.Host, test.Port, test.Password, test.UseTls, cert);
+                        await Console.Error.WriteLineAsync();
+
+                        await Console.Error.WriteLineAsync("via Pipelines...");
+                        await DoTheThingViaPipelines(pool, test.Host, test.Port, test.Password, test.UseTls, cert);
+                        await Console.Error.WriteLineAsync();
+                        await Console.Error.WriteLineAsync();
                     }
-
-                    await Console.Out.WriteLineAsync($"Test: {test.Name ?? test.Host}");
-                    if (cert != null)
+                    catch (Exception ex)
                     {
-                        await Console.Out.WriteLineAsync($"Client certificate: {cert.Subject}");
+                        await Console.Error.WriteLineAsync($"Error processing '{path}': '{ex.Message}'");
                     }
-
-                    await Console.Error.WriteLineAsync("via TcpClient...");
-                    // await DoTheThingViaTcpClient(pool, test.Host, test.Port, test.Password, test.UseTls, cert);
-                    await Console.Error.WriteLineAsync();
-
-                    await Console.Error.WriteLineAsync("via Pipelines...");
-                    await DoTheThingViaPipelines(pool, test.Host, test.Port, test.Password, test.UseTls, cert);
-                    await Console.Error.WriteLineAsync();
-                    await Console.Error.WriteLineAsync();
-                }
-                catch (Exception ex)
-                {
-                    await Console.Error.WriteLineAsync($"Error processing '{path}': '{ex.Message}'");
                 }
             }
         }
-    }
 
-    static async Task DoTheThingViaTcpClient(MemoryPool pool, string host, int port, string password, bool useTls, X509Certificate cert)
-    {
-        try
+        static async Task DoTheThingViaTcpClient(MemoryPool pool, string host, int port, string password, bool useTls, X509Certificate cert)
         {
-            using (var client = new TcpClient())
+            try
             {
-                await Console.Out.WriteLineAsync(ShowDetails ? $"connecting to {host}:{port}..." : "connecting to host");
-                await client.ConnectAsync(host, port);
-                Stream stream = client.GetStream();
-
-                if(useTls)
+                using (var client = new TcpClient())
                 {
-                    await Console.Out.WriteLineAsync($"authenticating host...");
-                    LocalCertificateSelectionCallback certSelector = null;
-                    if (cert != null)
+                    await Console.Out.WriteLineAsync(ShowDetails ? $"connecting to {host}:{port}..." : "connecting to host");
+                    await client.ConnectAsync(host, port);
+                    Stream stream = client.GetStream();
+
+                    if (useTls)
                     {
-                        certSelector = delegate { return cert; };
+                        await Console.Out.WriteLineAsync($"authenticating host...");
+                        LocalCertificateSelectionCallback certSelector = null;
+                        if (cert != null)
+                        {
+                            certSelector = delegate { return cert; };
+                        }
+                        RemoteCertificateValidationCallback serverValidator = delegate { return true; }; // WCGW?
+                        var ssl = new SslStream(stream, false, serverValidator, certSelector);
+                        await ssl.AuthenticateAsClientAsync(host);
+                        if (ssl.LocalCertificate != null)
+                        {
+                            Console.WriteLine($"Local cert: {ssl.LocalCertificate.Subject}");
+                        }
+                        stream = ssl;
                     }
-                    RemoteCertificateValidationCallback serverValidator = delegate { return true; }; // WCGW?
-                    var ssl = new SslStream(stream, false, serverValidator, certSelector);
-                    await ssl.AuthenticateAsClientAsync(host);
-                    if(ssl.LocalCertificate != null)
+
+                    using (var pipe = new StreamPipeConnection(new PipeOptions(pool), stream))
                     {
-                        Console.WriteLine($"Local cert: {ssl.LocalCertificate.Subject}");
+                        await ExecuteWithTimeout(pipe, password);
                     }
-                    stream = ssl;
                 }
-
-                using (var pipe = new StreamPipeConnection(new PipeOptions(pool), stream))
+            }
+            catch (Exception ex)
+            {
+                while (ex != null)
                 {
-                    await ExecuteWithTimeout(pipe, password);
+                    await Console.Error.WriteLineAsync(ex.Message);
+                    ex = ex.InnerException;
                 }
             }
         }
-        catch(Exception ex)
+        private static async Task ExecuteWithTimeout(IPipeConnection connection, string password, int timeoutMilliseconds = 5000)
         {
-            while(ex != null)
+            var timeout = Task.Delay(timeoutMilliseconds);
+            var success = Execute(connection, password);
+            var winner = await Task.WhenAny(success, timeout);
+            await Console.Out.WriteLineAsync(winner == success ? "(complete)" : "(timeout)");
+        }
+
+        private static async Task Execute(IPipeConnection connection, string password)
+        {
+            await Console.Out.WriteLineAsync($"executing...");
+
+            if (password != null)
+            {
+                await WriteSimpleMessage(connection.Output, $"AUTH \"{password}\"");
+                // a "success" for this would be a response that says "+OK"
+            }
+
+            await WriteSimpleMessage(connection.Output, "ECHO \"noisy in here\"");
+            // note that because of RESP, this actually gives 2 replies; don't worry about it :)
+
+            await WriteSimpleMessage(connection.Output, "PING");
+
+
+            var input = connection.Input;
+            while (true)
+            {
+                await Console.Out.WriteLineAsync($"awaiting response...");
+                var result = await input.ReadAsync();
+
+                await Console.Out.WriteLineAsync($"checking response...");
+                var buffer = result.Buffer;
+
+                if (buffer.IsEmpty && result.IsCompleted)
+                {
+                    await Console.Out.WriteLineAsync($"done");
+                    break;
+                }
+
+                if (!RespReply.TryParse(buffer, out var response, out var end))
+                {
+                    await Console.Out.WriteLineAsync($"incomplete");
+                    input.Advance(buffer.Start, buffer.End);
+                    continue;
+                }
+
+                var reply = response.ToString();
+                await Console.Out.WriteLineAsync($"<< received: '{reply}' ({response.Type})");
+                if (string.Equals(reply, "+PONG", StringComparison.OrdinalIgnoreCase))
+                {
+                    await WriteSimpleMessage(connection.Output, "QUIT");
+                    connection.Output.Complete();
+                }
+                input.Advance(end, end);
+            }
+        }
+
+        static async Task DoTheThingViaPipelines(MemoryPool pool, string host, int port, string password, bool useTls, X509Certificate cert)
+        {
+            try
+            {
+
+
+                await Console.Out.WriteLineAsync(ShowDetails ? $"connecting to '{host}:{port}'..." : "connecting to host");
+                using (var socket = await SocketTransportFactory.ConnectAsync(new DnsEndPoint(host, port), pool))
+                {
+                    IPipeConnection connection = socket;
+                    if (useTls) // need to think about the disposal story here?
+                    {
+                        await Console.Out.WriteLineAsync("authenticating client...");
+                        connection = await Leto.TlsPipeline.AuthenticateClient(connection, new Leto.ClientOptions());
+                        await Console.Out.WriteLineAsync("authenticated");
+                    }
+                    await ExecuteWithTimeout(connection, password);
+                }
+            }
+            catch (Exception ex)
             {
                 await Console.Error.WriteLineAsync(ex.Message);
-                ex = ex.InnerException;
-            }            
-        }
-    }
-    private static async Task ExecuteWithTimeout(IPipeConnection connection, string password, int timeoutMilliseconds = 5000)
-    {
-        var timeout = Task.Delay(timeoutMilliseconds);
-        var success = Execute(connection, password);
-        var winner = await Task.WhenAny(success, timeout);
-        await Console.Out.WriteLineAsync(winner == success ? "(complete)" : "(timeout)");
-    }
-    
-    private static async Task Execute(IPipeConnection connection, string password)
-    {
-        await Console.Out.WriteLineAsync($"executing...");
-
-        if (password != null)
-        {
-            await WriteSimpleMessage(connection.Output, $"AUTH \"{password}\"");
-            // a "success" for this would be a response that says "+OK"
+            }
         }
 
-        await WriteSimpleMessage(connection.Output, "ECHO \"noisy in here\"");
-        // note that because of RESP, this actually gives 2 replies; don't worry about it :)
-
-        await WriteSimpleMessage(connection.Output, "PING");
-
-
-        var input = connection.Input;
-        while (true)
+        private static async Task WriteSimpleMessage(IPipeWriter output, string command)
         {
-            await Console.Out.WriteLineAsync($"awaiting response...");
-            var result = await input.ReadAsync();
+            // keep things simple: using the text protocol
+            var msg = (!ShowDetails && command.StartsWith("AUTH", StringComparison.OrdinalIgnoreCase)) ? "AUTH ****" : command;
+            await Console.Out.WriteLineAsync($">> sending '{msg}'...");
+            var buffer = output.Alloc();
 
-            await Console.Out.WriteLineAsync($"checking response...");
-            var buffer = result.Buffer;
-
-            if (buffer.IsEmpty && result.IsCompleted)
-            {
-                await Console.Out.WriteLineAsync($"done");
-                break;
-            }
-
-            if (!buffer.TrySliceTo((byte)'\r', (byte)'\n', out var slice, out var cursor))
-            {
-                await Console.Out.WriteLineAsync($"incomplete");
-                input.Advance(buffer.Start, buffer.End);
-                continue;
-            }
-            var reply = slice.GetAsciiString();
-            await Console.Out.WriteLineAsync($"<< received: '{reply}'");
-            if (string.Equals(reply, "+PONG", StringComparison.OrdinalIgnoreCase))
-            {
-                await WriteSimpleMessage(connection.Output, "QUIT");
-                connection.Output.Complete();
-            }
-
-            // input.Advance(cursor); // feels like this should work, but it doesn't 
-            var incTerminator = buffer.Slice(0, slice.Length + 2);
-            input.Advance(incTerminator.End, incTerminator.End);
-
+            buffer.WriteUtf8(command.AsReadOnlySpan());
+            buffer.Ensure(CRLF.Length);
+            buffer.Write(CRLF);
+            buffer.Commit();
+            await buffer.FlushAsync();
         }
-    }
 
-    static async Task DoTheThingViaPipelines(MemoryPool pool, string host, int port, string password, bool useTls, X509Certificate cert)
-    {
-        try
+        static readonly byte[] CRLF = { (byte)'\r', (byte)'\n' };
+
+        private static int WriteUtf8(ref this WritableBuffer buffer, string value)
+            => buffer.WriteUtf8(value.AsReadOnlySpan());
+        private static int WriteUtf8(ref this WritableBuffer buffer, ReadOnlySpan<char> value)
         {
 
+            if (value.IsEmpty) return 0;
 
-            await Console.Out.WriteLineAsync(ShowDetails ? $"connecting to '{host}:{port}'..." : "connecting to host");
-            using (var socket = await SocketTransportFactory.ConnectAsync(new DnsEndPoint(host, port), pool))
+            int totalWritten = 0;
+            var source = value.AsBytes();
+            do
             {
-                IPipeConnection connection = socket;
-                if (useTls) // need to think about the disposal story here?
+                buffer.Ensure(4); // be able to write at least one character (worst case) - but the span obtained could be much bigger
+                var status = Encodings.Utf8.FromUtf16(source, buffer.Buffer.Span, out int bytesConsumed, out int bytesWritten);
+                switch (status)
                 {
-                    await Console.Out.WriteLineAsync("authenticating client...");
-                    connection = await Leto.TlsPipeline.AuthenticateClient(connection, new Leto.ClientOptions());
-                    await Console.Out.WriteLineAsync("authenticated");
+                    case OperationStatus.Done:
+                    case OperationStatus.DestinationTooSmall:
+                        if (bytesWritten == 0) ThrowInvalid("Zero bytes encoded");
+
+                        buffer.Advance(bytesWritten);
+                        source = source.Slice(bytesConsumed);
+                        totalWritten += bytesWritten;
+                        break;
+                    default:
+                        ThrowInvalid($"Unexpected encoding status: {status}");
+                        break;
                 }
-                await ExecuteWithTimeout(connection, password);
-            }
+            } while (!source.IsEmpty);
+            return totalWritten;
         }
-        catch (Exception ex)
-        {
-            await Console.Error.WriteLineAsync(ex.Message);
-        }
+        static void ThrowInvalid(string message) => throw new InvalidOperationException(message);
     }
-
-    private static async Task WriteSimpleMessage(IPipeWriter output, string command)
-    {
-        // keep things simple: using the text protocol guarantees a text-protocol response
-        // (in real code we'd use the RESP format, but ... meh)
-        var msg = (!ShowDetails && command.StartsWith("AUTH", StringComparison.OrdinalIgnoreCase)) ? "AUTH ****" : command;
-        await Console.Out.WriteLineAsync($">> sending '{msg}'...");
-        var buffer = output.Alloc();
-
-        buffer.WriteUtf8(command.AsReadOnlySpan());
-        buffer.Ensure(CRLF.Length);
-        buffer.Write(CRLF);
-        buffer.Commit();
-        await buffer.FlushAsync();
-    }
-
-    static readonly byte[] CRLF = { (byte)'\r', (byte)'\n' };
-
-    private static int WriteUtf8(ref this WritableBuffer buffer, string value)
-        => buffer.WriteUtf8(value.AsReadOnlySpan());
-    private static int WriteUtf8(ref this WritableBuffer buffer, ReadOnlySpan<char> value)
-    {
-               
-        if (value.IsEmpty) return 0;
-
-        int totalWritten = 0;
-        var source = value.AsBytes();
-        do
-        {
-            buffer.Ensure(4); // be able to write at least one character (worst case) - but the span obtained could be much bigger
-            var status = Encodings.Utf8.FromUtf16(source, buffer.Buffer.Span, out int bytesConsumed, out int bytesWritten);
-            switch(status)
-            {
-                case OperationStatus.Done:
-                case OperationStatus.DestinationTooSmall:
-                    if (bytesWritten == 0) ThrowInvalid("Zero bytes encoded");
-
-                    buffer.Advance(bytesWritten);
-                    source = source.Slice(bytesConsumed);
-                    totalWritten += bytesWritten;
-                    break;
-                default:
-                    ThrowInvalid($"Unexpected encoding status: {status}");
-                    break;
-            }
-        } while (!source.IsEmpty);
-        return totalWritten;        
-    }
-    static void ThrowInvalid(string message) => throw new InvalidOperationException(message);
 }
---
+++ RedisPing/RespReply.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO.Pipelines;
+using System.IO.Pipelines.Text.Primitives;
+
+namespace RedisPing
+{
+    enum MessageType : byte
+    {
+        SimpleString = (byte)'+',
+        Error = (byte)'-',
+        Integer = (byte)':',
+        BulkString = (byte)'$',
+        Array = (byte)'*',
+    }
+    struct RespReply
+    {
+        public MessageType Type { get; }
+        private ReadableBuffer _payload;
+        private RespReply[] _subItems;
+
+        public override string ToString() => AsString();
+        public string AsString() => _payload.GetUtf8Span();
+
+        private RespReply(MessageType type, ReadableBuffer payload, RespReply[] subItems = null)
+        {   // note that **these buffers are not preserved**; they cannot be used once we have advanced
+            Type = type;
+            _payload = payload;
+            _subItems = subItems;
+        }
+        
+        private static bool TrySliceToCrLf(ReadableBuffer value, out ReadableBuffer slice, out ReadCursor end)
+        {
+            bool success = value.TrySliceTo((byte)'\r', (byte)'\n', out slice, out _);
+            // the cursor from TrySliceTo is the CRLF - we want to return the *after* position
+            end = success ? value.Slice(slice.Length + 2).Start : default;
+            return success;
+        }
+        static readonly Memory<byte> CRLF = new byte[] { (byte)'\r', (byte)'\n' };
+
+        public static bool TryParse(ReadableBuffer buffer, out RespReply value, out ReadCursor end)
+        {
+            end = default;
+            value = default;
+            if (buffer.IsEmpty) return false;
+
+            // RESP type is denoted by prefix char
+            var type = (MessageType)buffer.First.Span[0];
+            buffer = buffer.Slice(1); // consumed
+
+            switch (type)
+            {
+                case MessageType.SimpleString:
+                case MessageType.Error:
+                case MessageType.Integer:
+                    // simple value followed by CRLF
+                    if (!TrySliceToCrLf(buffer, out var payload, out end)) return false;
+                    value = new RespReply(type, payload);
+                    return true;
+                case MessageType.BulkString:
+                    // length as ASCII followed by CRLF, then that many bytes, then CRLF
+                    if (!TrySliceToCrLf(buffer, out payload, out end)) return false;
+                    int len = checked((int)ReadableBufferExtensions.GetUInt32(payload));
+                    //int len = ParseInt32(payload);
+                    buffer = buffer.Slice(end);
+                    if (buffer.Length < len + 2) return false; // payload+CRLF bytes not present
+
+                    payload = buffer.Slice(0, len);
+                    var crlf = buffer.Slice(len, 2);
+                    if (!crlf.EqualsTo(CRLF.Span)) throw new FormatException("Missing terminator after bulk-string");
+                    end = crlf.End;
+                    value = new RespReply(type, payload);
+                    return true;
+                case MessageType.Array:
+                    // haven't needed yet for my test scenario
+                    throw new NotImplementedException(type.ToString());
+                default:
+                    throw new InvalidProgramException($"Unknown message prefix: {(char)type}");
+            }
+        }
+
+    }
+}
